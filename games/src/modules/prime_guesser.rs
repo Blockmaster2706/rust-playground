d1df76699a619651eb0767d508c8db985b8f7053
Blockmaster2706 <blockyork@live.de>
2024-06-14 20:47:44 +0200

    Make prime guesser better

--- games/src/modules/prime_guesser.rs
+++ games/src/modules/prime_guesser.rs
@@ -1,10 +1,20 @@
 use colored::Colorize;
+use std::collections::HashMap;
+
+
 
 pub fn run_prime_guesser() {
     let mut prime = 2;
     let mut primes = vec![];
+    let mut correctly_guessed = 0;
 
     print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+
+    let prime_hints: HashMap<u32, &str> = [
+        (2, "What game number is this game?"),
+        
+        // Add more primes and their hints here
+    ].iter().cloned().collect();
 
     loop {
 
@@ -17,7 +27,15 @@ pub fn run_prime_guesser() {
         }
 
         if is_prime_number(&prime) {
-            println!("What is the next prime?");
+            if primes.len() > 0 {
+                println!("What is the next prime after {}?", primes.last().unwrap());
+            } else {
+                println!("What is the next prime?");
+            }
+            match prime_hints.get(&prime) {
+                Some(hint) => println!("Hint: {}", hint),
+                None => {},
+            }
             
             let mut guess = String::new();
             std::io::stdin()
@@ -41,10 +59,12 @@ pub fn run_prime_guesser() {
 
             if guess == prime {
                 primes.push(prime);
-                println!("{} You have found {} prime numbers.", "Correct!".to_string().green(), primes.len());
+                correctly_guessed += 1;
+                println!("{} You have found {} prime numbers.", "Correct!".to_string().green(), correctly_guessed);
             } else {
-                println!("{} The next prime is {}.", "Incorrect!".to_string().red(), prime);
-                println!("You have found {} prime numbers.", primes.len());
+                primes.push(prime);
+                println!("{}, {} The next prime is {}.", "Incorrect!".to_string().red(), is_not_prime_number_because(&guess), prime);
+                println!("You have found {} prime numbers.", correctly_guessed);
             }
             println!("The Prime Numbers so far were: {}", primes.iter().map(|p| p.to_string()).collect::<Vec<String>>().join(", "));
         }
@@ -64,4 +84,17 @@ fn is_prime_number(num: &u32) -> bool {
         }
     }
     true
+}
+
+fn is_not_prime_number_because(num: &u32) -> String {
+    if *num <= 1 {
+        return format!("because {} is less than 2.", num).to_string();
+    }
+    
+    for p in 2..*num {
+        if num % p == 0 {
+            return format!("because {}'s lowest divider is {}.", num, p).to_string();
+        }
+    }
+    return format!("because {} is a later prime number.", num).to_string();
 }
